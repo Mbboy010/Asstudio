275c82858e23866b99c5831e8bb70b213cd2eed6
mbboy010 <mbboy010@gmail.com>
2026-02-14 23:59:22 +0100

    Initial commit

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -83,7 +83,7 @@ export default async function Page({ params }: Props) {
 
   return (
     <main>
-      <ProductDetailContent product={product} />
+      <ProductDetailContent />
     </main>
   );
 }
