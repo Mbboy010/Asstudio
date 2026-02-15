6f7c361f7abf6b5b1829282171534a5e9a792710
mbboy010 <mbboy010@gmail.com>
2026-02-15 07:53:23 +0100

    Initial commit

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 import { adminDb } from '@/lib/firebaseAdmin';
-import ProductDetailContent from "@/pages/ProductDetailContent";
+import ProductDetailContent from "@/components/ProductDetailContent";
 
 interface Product {
   id: string;
