f40a0c07464495f302eb6f01ae1df3fb44abd83a
mbboy010 <mbboy010@gmail.com>
2026-07-02 07:24:49 +0100

    Initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,14 +62,19 @@ export default function RootLayout({
     <html lang="en" className="dark" suppressHydrationWarning>
       <head>
         {/* 3. Official Google AdSense Integration */}
-        <Script
+
+      </head>
+      <body className={`${inter.variable} ${firaCode.variable} font-sans`}>
+
+
+       <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9241182560906060"
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
-      </head>
-      <body className={`${inter.variable} ${firaCode.variable} font-sans`}>
+
+
         
         <AuthProvider>
           <AuthGuard>
